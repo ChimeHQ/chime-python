97cdf3b2e310133f28bebece1f155d5680d1cf13
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-07-26 06:39:08 -0400

    Update to ChimeKit 0.3.0

--- Sources/ChimePython/PythonExtension.swift
+++ Sources/ChimePython/PythonExtension.swift
@@ -2,49 +2,24 @@ import Foundation
 
 import ChimeKit
 
+@MainActor
 public final class PythonExtension {
 	private let lspService: LSPService
 
-	public init(host: any HostProtocol, processHostServiceName: String) {
+	public init(host: any HostProtocol) {
 		self.lspService = LSPService(host: host,
-									 executableName: "pylsp",
-									 processHostServiceName: processHostServiceName)
+									 executableName: "pylsp")
 	}
 }
 
 extension PythonExtension: ExtensionProtocol {
 	public var configuration: ExtensionConfiguration {
-		get async throws {
-			return ExtensionConfiguration(contentFilter: [.uti(.pythonScript)])
+		get throws {
+			ExtensionConfiguration(contentFilter: [.uti(.pythonScript)])
 		}
 	}
-
-	public func didOpenProject(with context: ProjectContext) async throws {
-		try await lspService.didOpenProject(with: context)
-	}
-
-	public func willCloseProject(with context: ProjectContext) async throws {
-		try await lspService.willCloseProject(with: context)
-	}
-
-	public func symbolService(for context: ProjectContext) async throws -> SymbolQueryService? {
-		return try await lspService.symbolService(for: context)
-	}
-
-	public func didOpenDocument(with context: DocumentContext) async throws -> URL? {
-		return try await lspService.didOpenDocument(with: context)
-	}
-
-	public func didChangeDocumentContext(from oldContext: DocumentContext, to newContext: DocumentContext) async throws {
-		return try await lspService.didChangeDocumentContext(from: oldContext, to: newContext)
-	}
-
-	public func willCloseDocument(with context: DocumentContext) async throws {
-		return try await lspService.willCloseDocument(with: context)
-	}
-
-	public func documentService(for context: DocumentContext) async throws -> DocumentService? {
-		return try await lspService.documentService(for: context)
+	
+	public var applicationService: ApplicationService {
+		return lspService
 	}
 }
-
